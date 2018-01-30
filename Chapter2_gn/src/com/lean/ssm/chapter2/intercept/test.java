b0f153239ec284201a02692494264a7b0a7f30a5
dqaeswGN <15645430925@163.com>
2018-01-30 10:23:57 +0800

    测试责任链模式

--- Chapter2_gn/src/com/lean/ssm/chapter2/intercept/test.java
+++ Chapter2_gn/src/com/lean/ssm/chapter2/intercept/test.java
@@ -6,7 +6,7 @@ import com.lean.ssm.chapter2.proxy.HelloWorldImpl;
 public class test {
 
 	public static void main(String[] args) {
-		testinterceptor();
+		testChain();
 
 	}
 
@@ -14,4 +14,17 @@ public class test {
 		HelloWorld hw=(HelloWorld)InterceptorJdkProxy.bind(new HelloWorldImpl(), "com.lean.ssm.chapter2.intercept.MyInterceptor");
 		hw.sayhello();
 	}
+	
+	
+	
+	public static void testChain() {
+		HelloWorld proxy1 = (HelloWorld) InterceptorJdkProxy.bind(
+                new HelloWorldImpl(), "com.lean.ssm.chapter2.intercept.Interceptor1");
+        HelloWorld proxy2 = (HelloWorld) InterceptorJdkProxy.bind(
+                proxy1, "com.lean.ssm.chapter2.intercept.Interceptor2");
+        HelloWorld proxy3 = (HelloWorld) InterceptorJdkProxy.bind(
+                proxy2, "com.lean.ssm.chapter2.intercept.Interceptor3");
+        proxy3.sayhello();
+	}
+	
 }
